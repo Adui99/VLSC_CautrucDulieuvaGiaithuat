21d5886fcea106e7aa3b7ce2b00c4d562af1f259
Adui99 <2500114656@vanlangsaigon.edu.vn>
2026-03-16 10:21:02 +0700

    Hoan thanh BTVN buoi 2: De quy va so sanh bo nho

---
+++ DSA_Homework_S2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DSA_Homework_S2
+{
+    class Program
+    {
+        //Ham de quy tinh tong S(n)
+        static int TinhTong(int n)
+        {
+            if (n == 1) return 1;
+
+            return n + TinhTong(n - 1);
+        }
+
+        //Ham giai thua - loop
+        static long GiaiThuaVongLap(int n)
+        {
+            long ketQua = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                ketQua *= i;
+            }
+            return ketQua;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Nhap n de tinh tong: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Tong S({n}) = {TinhTong(n)}");
+            Console.WriteLine($"Giai thua cua {n} la {GiaiThuaVongLap(n)}");
+
+        }
+    }
+}
